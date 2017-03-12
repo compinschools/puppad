0ac366703df2f523a8eadbeb161787a6c8e2e8da
Colin Smith <csmith@utcsheffield.org.uk>
2017-03-12 20:42:23 +0000

    Add project files.

---
+++ PupPad/src/gameStates/padmain.ts
@@ -0,0 +1,170 @@
+﻿module PupPad.Client {
+
+    export class PadMain extends Phaser.State {
+
+        maxBadge = 5;
+        maxChase = 2;
+        maxMarshall = 2;
+        maxRocky = 1;
+        maxRubble = 1;
+        maxSkye = 1;
+        maxZuma = 2;
+
+
+        currentBadge = 5;
+        currentChase = 2;
+        currentMarshall = 2;
+        currentRocky = 1;
+        currentRubble = 1;
+        currentSkye = 1;
+        currentZuma = 2;
+
+        background: Phaser.Sprite;
+        //logo: Phaser.Sprite;
+        chase: Phaser.Sprite;
+        marshall: Phaser.Sprite;
+        rubble: Phaser.Sprite;
+        zuma: Phaser.Sprite;
+        rocky: Phaser.Sprite;
+        skye: Phaser.Sprite;
+        badge: Phaser.Sprite;
+
+        currentSound: Phaser.Sound;
+
+        create() {
+            //this.background = this.add.sprite(0, 0, 'background');
+            //this.background.alpha = 0;
+
+            //this.logo = this.add.sprite(this.world.centerX, -300, 'logo');
+            //this.logo.anchor.setTo(0.5);
+
+            //this.add.tween(this.background).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true);
+            //this.add.tween(this.logo).to({ y: 220 }, 2000, Phaser.Easing.Elastic.Out, true, 500);
+
+            this.chase = this.add.sprite(this.world.centerX, 75, 'chase');
+            this.chase.anchor.setTo(0.5);
+            this.chase.inputEnabled = true;
+            this.chase.events.onInputDown.add(this.chaseVoice, this);
+                        //this.game.debug.text("Click the logo to start the game", 0, this.world.height, "red");
+
+            this.skye = this.add.sprite(this.world.centerX, 600, 'skye');
+            this.skye.anchor.setTo(0.5);
+            this.skye.inputEnabled = true;
+            this.skye.events.onInputDown.add(this.skyeVoice, this);
+
+            this.marshall = this.add.sprite(this.world.centerX + 225, 225, 'marshall');
+            this.marshall.anchor.setTo(0.5);
+            this.marshall.inputEnabled = true;
+            this.marshall.events.onInputDown.add(this.marshallVoice, this);
+
+            this.rubble = this.add.sprite(this.world.centerX + 225, 450, 'rubble');
+            this.rubble.anchor.setTo(0.5);
+            this.rubble.inputEnabled = true;
+            this.rubble.events.onInputDown.add(this.rubbleVoice, this);
+
+            this.rocky = this.add.sprite(this.world.centerX - 225, 225, 'rocky');
+            this.rocky.anchor.setTo(0.5);
+            this.rocky.inputEnabled = true;
+            this.rocky.events.onInputDown.add(this.rockyVoice, this);
+
+            this.zuma = this.add.sprite(this.world.centerX - 225, 450, 'zuma');
+            this.zuma.anchor.setTo(0.5);
+            this.zuma.inputEnabled = true;
+            this.zuma.events.onInputDown.add(this.zumaVoice, this);
+
+            this.badge = this.add.sprite(this.world.centerX, 300, 'badge');
+            this.badge.anchor.setTo(0.5);
+            this.badge.inputEnabled = true;
+            this.badge.events.onInputDown.add(this.badgeVoice, this);
+
+            this.currentSound = this.add.audio('chaseAudio1', 1, false);
+            //this.input.onDown.addOnce(this.fadeOut, this);
+        }
+
+        badgeVoice() {
+
+            this.currentBadge++;
+            if (this.currentBadge > this.maxBadge) this.currentBadge = 1;
+
+            this.currentSound.stop();
+            this.currentSound = this.add.audio('badgeAudio' + this.currentBadge.toString(), 1, false);
+            this.currentSound.play();
+            //this.game.debug.text("Badge Audio 1", 0, this.world.height, "red");
+        }
+
+        chaseVoice() {
+
+            this.currentChase++;
+            if (this.currentChase > this.maxChase) this.currentChase = 1;
+            
+            this.currentSound.stop();
+            this.currentSound = this.add.audio('chaseAudio' + this.currentChase.toString(), 1, false);
+            this.currentSound.play();
+            //this.game.debug.text("Chase Audio 1", 0, this.world.height, "red");
+        }
+
+        marshallVoice() {
+            this.currentMarshall++;
+            if (this.currentMarshall > this.maxMarshall) this.currentMarshall = 1;
+
+            this.currentSound.stop();
+            this.currentSound = this.add.audio('marshallAudio' + this.currentMarshall.toString(), 1, false);
+            this.currentSound.play();
+            //this.game.debug.text("marshall Audio 1", 0, this.world.height, "red");
+        }
+
+        rubbleVoice() {
+            this.currentRubble++;
+            if (this.currentRubble > this.maxRubble) this.currentRubble = 1;
+
+            this.currentSound.stop();
+            this.currentSound = this.add.audio('rubbleAudio' + this.currentRubble.toString(), 1, false);
+            this.currentSound.play();
+            //this.game.debug.text("rubble Audio 1", 0, this.world.height, "red");
+        }
+
+        skyeVoice() {
+            this.currentSkye++;
+            if (this.currentSkye > this.maxSkye) this.currentSkye = 1;
+
+            this.currentSound.stop();
+            this.currentSound = this.add.audio('skyeAudio' + this.currentSkye.toString(), 1, false);
+            this.currentSound.play();
+            //this.game.debug.text("skye Audio 1", 0, this.world.height, "red");
+        }
+
+        rockyVoice() {
+            this.currentRocky++;
+            if (this.currentRocky > this.maxRocky) this.currentRocky = 1;
+
+            this.currentSound.stop();
+            this.currentSound = this.add.audio('rockyAudio' + this.currentRocky.toString(), 1, false);
+            this.currentSound.play();
+            //this.game.debug.text("rocky Audio 1", 0, this.world.height, "red");
+        }
+
+        zumaVoice() {
+            this.currentZuma++;
+            if (this.currentZuma > this.maxZuma) this.currentZuma = 1;
+
+            this.currentSound.stop();
+            this.currentSound = this.add.audio('zumaAudio' + this.currentZuma.toString(), 1, false);
+            this.currentSound.play();
+            //this.game.debug.text("zuma Audio 1", 0, this.world.height, "red");
+        }
+
+        /*fadeOut() {
+            this.add.audio('click', 1, false).play();
+            
+            this.add.tween(this.background).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+            var tween = this.add.tween(this.logo).to({ y: 800 }, 2000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startGame, this);
+        }
+
+        startGame() {
+            this.game.state.start('Level01', true, false);
+        }*/
+
+    }
+
+}
---
+++ PupPad/src/gameStates/preloader.ts
@@ -0,0 +1,70 @@
+﻿module PupPad.Client {
+
+    export class Preloader extends Phaser.State {
+        loaderText: Phaser.Text;
+        assetLocation: string = "";
+        boot() {
+           
+        }
+        preload() {
+            
+            this.loaderText = this.game.add.text(this.world.centerX, 200, "Loading...",
+                { font: "18px Arial", fill: "#A9A91111", align: "center" });
+            this.loaderText.anchor.setTo(0.5);
+            //this.game.load.baseURL = 'http://utcsheffield.azurewebsites.net/pupPad/';
+            //this.game.load.crossOrigin = 'anonymous';
+            //this.game.load.image('background', this.assetLocation + 'assets/ui/background.jpg');
+            this.game.load.image('chase', this.assetLocation + 'assets/sprites/111.png');
+            this.game.load.image('marshall', this.assetLocation + 'assets/sprites/222.png');
+            this.game.load.image('rocky', this.assetLocation + 'assets/sprites/rocky.png');
+            this.game.load.image('rubble', this.assetLocation + 'assets/sprites/rubble.png');
+            this.game.load.image('skye', this.assetLocation + 'assets/sprites/333.png');
+            this.game.load.image('zuma', this.assetLocation + 'assets/sprites/444.png');
+            this.game.load.image('badge', this.assetLocation + 'assets/sprites/badge.png');
+
+            //this.load.image('logo', this.assetLocation + 'assets/ui/gameLogo.png');
+            //this.load.audio('click', this.assetLocation + 'assets/sounds/click.ogg', true);
+            this.game.load.audio('badgeAudio1', this.assetLocation + 'assets/sounds/badgeAudio1.ogg');
+            this.game.load.audio('badgeAudio2', this.assetLocation + 'assets/sounds/badgeAudio2.ogg');
+            this.game.load.audio('badgeAudio3', this.assetLocation + 'assets/sounds/badgeAudio3.ogg');
+            this.game.load.audio('badgeAudio4', this.assetLocation + 'assets/sounds/badgeAudio4.ogg');
+            this.game.load.audio('badgeAudio5', this.assetLocation + 'assets/sounds/badgeAudio5.ogg');
+
+            this.game.load.audio('chaseAudio1', this.assetLocation + 'assets/sounds/chaseAudio1.ogg');
+            this.game.load.audio('chaseAudio2', this.assetLocation + 'assets/sounds/chaseAudio2.ogg');
+
+            this.game.load.audio('marshallAudio1', this.assetLocation + 'assets/sounds/marshallAudio1.ogg');
+            this.game.load.audio('marshallAudio2', this.assetLocation + 'assets/sounds/marshallAudio2.ogg');
+
+            this.game.load.audio('rockyAudio1', this.assetLocation + 'assets/sounds/rockyAudio1.ogg');
+
+            this.game.load.audio('rubbleAudio1', this.assetLocation + 'assets/sounds/rubbleAudio1.ogg');
+
+            this.game.load.audio('skyeAudio1', this.assetLocation + 'assets/sounds/skyeAudio1.ogg');
+
+            this.game.load.audio('zumaAudio1', this.assetLocation + 'assets/sounds/zumaAudio1.ogg');
+            this.game.load.audio('zumaAudio2', this.assetLocation + 'assets/sounds/zumaAudio2.ogg');
+            this.game.load.onLoadComplete.add(this.vanish, this);
+            //this.load.atlasJSONHash('level01-sprites', '.assets/sprites/level01-sprites.png', '.assets/sprites/level01-sprites.json');
+        }
+
+        create() {
+           
+           
+        }
+
+        vanish() {
+            while (!this.game.load.hasLoaded) {
+
+            }
+             var tween = this.add.tween(this.loaderText).to({ alpha: 0 }, 1000,
+                Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startMainMenu, this);
+        }
+        startMainMenu() {
+            this.game.state.start('PadMain', true, false);
+        }
+
+    }
+
+}
